297a5c83045dbcb889670c6fa74551fcd7ce3328
walid <walid@gmail.com>
2022-05-30 14:38:33 +0200

    Edit Update page

--- src/Reducers/AddProduct.js
+++ src/Reducers/AddProduct.js
@@ -6,9 +6,9 @@ import { ADD_PRODUCT, UPDATE_PRODUCT } from "../ActionConst/ActionConst";
 export default function AddProduct(state = [], {type,payload}) {
     switch (type) {
         case ADD_PRODUCT:
-            return ;
+            return state;
         case UPDATE_PRODUCT:
-            return ;
+            return state;
         default:
             return state;
     }
